b4522a344553a2c18689bc0c5226b37ded46fa3e
arafatah <arafathowlader890@gmail.com>
2024-12-29 15:44:01 +0600

    initial class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const scenes = document.querySelectorAll(".scene");
+/* const scenes = document.querySelectorAll(".scene");
+
 // initial styles
 scenes.forEach((scene) => {
   scene.style.position = "relative";
@@ -8,7 +9,7 @@ scenes.forEach((scene) => {
 });
 
 document.addEventListener("mousemove", (e) => {
-  // mouse position relative to the center of the viewport
+  // mouse position 
   const xPos = (e.clientX - window.innerWidth / 2) / 100;
   const yPos = (e.clientY - window.innerHeight / 2) / 100;
 
@@ -17,14 +18,45 @@ document.addEventListener("mousemove", (e) => {
     scene.style.transform = `translate3d(${xPos * depth}px, ${yPos * depth}px, 0)`;
     scene.style.transformStyle = "preserve-3d";
     scene.style.backfaceVisibility = "hidden";
-    scene.style.transition = "transform 0.1s ease-out";
+    scene.style.transition = "all 0.3s ease-out";
   });
-
-  // Update the container's position for a subtle overall movement
-  const container = document.querySelector(".container");
-  container.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
-  container.style.transformStyle = "preserve-3d";
-  container.style.backfaceVisibility = "hidden";
-  container.style.transition = "transform 0.1s ease-out";
 });
+ */
 
+// With class - Original
+class Parallax {
+  constructor() {
+    this.scenes = document.querySelectorAll(".scene");
+    this.init();
+  }
+
+  init() {
+    this.scenes.forEach((scene) => {
+      scene.style.position = "relative";
+      scene.style.transform = "translate3d(0px, 0px, 0px)";
+      scene.style.transformStyle = "preserve-3d";
+      scene.style.backfaceVisibility = "hidden";
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      this.update(e);
+    });
+  }
+
+  update(e) {
+    const xPos = (e.clientX - window.innerWidth / 2) / 100;
+    const yPos = (e.clientY - window.innerHeight / 2) / 100;
+
+    this.scenes.forEach((scene) => {
+      const depth = scene.dataset.depth || 1;
+      scene.style.transform = `translate3d(${xPos * depth}px, ${
+        yPos * depth
+      }px, 0)`;
+      scene.style.transformStyle = "preserve-3d";
+      scene.style.backfaceVisibility = "hidden";
+      scene.style.transition = "all 0.3s ease-out";
+    });
+  }
+}
+
+new Parallax();
